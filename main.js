c5398a6697470488826d97cb8bed1a3fcc884291
Peter Surrena <peter@f-m.fm>
2025-09-29 23:52:51 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Import main scss
+import './scss/app.scss';
+
+import '/js/panel.js';
+
+console.log("FoHL was LIT!");
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { globSync } from 'glob';
+
+export default defineConfig({
+
+  define: {
+    global: {},
+  },
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        globSync('**/*.html', { ignore: ['dist/**', 'node_modules/**'] }).map(file => [
+          // This part generates the correct output name for the file
+          file.slice(0, file.length - '.html'.length),
+          resolve(__dirname, file)
+        ])
+      ),
+    },
+  },
+});
